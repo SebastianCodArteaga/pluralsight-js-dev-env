a7defea77b5a5311076257f3de08bad959c41bad
Sebastian Arteaga <sebastian.arteaga@somoglobal.com>
2020-04-22 14:40:12 -0500

    Se cambia el titulo con deseo de funcionar JS

---
+++ home.js
@@ -0,0 +1,6 @@
+function changeTittle(message){
+
+    document.getElementById('title').textContent = message + 'Estamos putamente trabajando';
+}
+
+changeTittle('VAmos a Romperla Mi hermano'+' ');
